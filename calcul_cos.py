430df7bf501cab476fceb806bfb708b2a1865cc6
Maxime296 <maximehodllab@gmail.com>
2024-12-31 14:14:30 +0100

    Add files via upload

---
+++ calcul_cos.py
@@ -0,0 +1,5 @@
+import math
+
+angle = float(input("Entrez un angle en radians : "))
+result = math.cos(angle)
+print(f"Cosinus de {angle} = {result}")
---
+++ calcul_sin.py
@@ -0,0 +1,5 @@
+import math
+
+angle = float(input("Entrez un angle en radians : "))
+result = math.sin(angle)
+print(f"Sinus de {angle} = {result}")
